e47e8c84b00db31e73b65cb23f0e2acd198ece3e
sillysillyman <sillysillyman.cs@gmail.com>
2024-09-02 17:27:23 +0900

    init commit

---
+++ src/main/java/com/example/preonboardingbackendassignment/PreonboardingBackendAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.preonboardingbackendassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreonboardingBackendAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreonboardingBackendAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/preonboardingbackendassignment/PreonboardingBackendAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preonboardingbackendassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreonboardingBackendAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
