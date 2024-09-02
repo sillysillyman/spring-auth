7ad51febf03c10ad3e27ea1a2065f9bb5afefc98
sillysillyman <sillysillyman.cs@gmail.com>
2024-09-02 21:24:53 +0900

    feat: JPA 감사 기능 활성화를 위한 @EnableJpaAuditing 추가

--- src/main/java/com/example/springauth/SpringAuthApplication.java
+++ src/main/java/com/example/springauth/SpringAuthApplication.java
@@ -2,12 +2,14 @@ package com.example.springauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringAuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthApplication.class, args);
+    }
 
 }
