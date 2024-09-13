aa0254c2d26e6fd1dd0fd279c395a2ff8dcc29f6
sillysillyman <sillysillyman.cs@gmail.com>
2024-09-13 14:21:01 +0900

    feat: null 검사 추가

--- src/main/java/com/example/springauth/domain/user/dto/UserResponseDto.java
+++ src/main/java/com/example/springauth/domain/user/dto/UserResponseDto.java
@@ -5,6 +5,10 @@ import com.example.springauth.domain.user.entity.User;
 public record UserResponseDto(String username, String email) {
 
     public static UserResponseDto from(User user) {
+        if (user == null) {
+            throw new IllegalArgumentException("사용자는 null일 수 없습니다.");
+        }
+
         return new UserResponseDto(user.getUsername(), user.getEmail());
     }
 }
